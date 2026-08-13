ac8170357b49d9485ac1f1879d84aafd2b197705
beat <beat@faeh.sh>
2026-08-13 12:05:54 +0200

    goUp

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'host' => getenv('BUDDHISMUS_DB_HOST') ?: 'localhost',
-    'username' => getenv('BUDDHISMUS_DB_USERNAME') ?: 'absolutions_usr',
-    'password' => getenv('BUDDHISMUS_DB_PASSWORD') ?: '4m29$2Dhp',
-    'database' => getenv('BUDDHISMUS_DB_NAME') ?: 'absolutions_db',
+    'username' => getenv('BUDDHISMUS_DB_USERNAME') ?: 'buddha_usr',
+    'password' => getenv('BUDDHISMUS_DB_PASSWORD') ?: 'YEh_66Sb0smi*lxe',
+    'database' => getenv('BUDDHISMUS_DB_NAME') ?: 'buddha_db',
 ];
